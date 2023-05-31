c542b9700d7500854f264e91263388aacf59ab13
Shirin_Musaeva <shirinmusaeva26@gmail.com>
2023-05-31 14:47:20 +0600

    update fixtures

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -3,10 +3,11 @@ from webapp.models import Product
 
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name', 'category', 'balance', 'price']
+    list_display = ['name', 'category', 'balance', 'price']
     list_display_links = ['name']
     list_filter = ['category']
     search_fields = ['name']
-    fields = ['id', 'name', 'description', 'category', 'balance', 'price']
+    fields = ['name', 'description', 'category', 'balance', 'price']
+
 
 admin.site.register(Product, ProductAdmin)
