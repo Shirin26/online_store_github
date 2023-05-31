1274b013d01db6842f69f67ddc559a7b7b250f9c
Shirin_Musaeva <shirinmusaeva26@gmail.com>
2023-05-31 14:16:48 +0600

    applied migrations, created superuser, registered in admin

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from webapp.models import Product
 
-# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'category', 'balance', 'price']
+    list_display_links = ['name']
+    list_filter = ['category']
+    search_fields = ['name']
+    fields = ['id', 'name', 'description', 'category', 'balance', 'price']
+
+admin.site.register(Product, ProductAdmin)
